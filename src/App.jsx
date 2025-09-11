05d81e9128e45ba6e66cdf972db049e06433a5b4
AdrielSolano <2302019@utrivieramaya.edu.mx>
2025-09-11 16:00:13 -0500

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Navbar from './components/Navbar';
 
 export default function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return 
+  
+  <Navbar />;
 }
---
+++ src/router/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+
+// Importar componentes
+
+
+export default function AppRoutes() {
+    return (
+
+        <>  
+        
+        </>
+
+    )
+}
